4e45410d1756f65b8db99e4adba57183bb4927f5
Jeff Lin <thejefflin@gmail.com>
2022-05-18 11:26:25 -0400

    pass fetch data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-function fetchPokemon() {
+async function fetchPokemon() {
         for(let i = 1; i <= 151; i++) {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         .then(res => res.json())
         .then( data => {
             const pokemon = {};
@@ -18,17 +18,27 @@ function fetchPokemon() {
     }
 }
 
-const pokedex = document.getElementById("pokedex")
-
 function displayPokemon(pokemon) {
-    const pokemonHTML = pokemon
-    .map((pokemon) => 
-    pokemon.image,
-    pokemon.id,
-    pokemon.name,
-    pokemon.type
-    ).join(" ");
-    pokedex.innerHTML = pokemonHTML;
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const id = pokemon.id;
+    const type = (pokemon.type[0].toUpperCase() + pokemon.type.slice(1)).split(",")[0];
+
+    const img = document.createElement("img");
+    img.src = pokemon.image;
+    document.body.append(img)
+
+    const pokemonID = document.createElement("div");
+    pokemonID.innerHTML = id;
+    document.body.append(pokemonID);
+
+    const pokemonName = document.createElement("div");
+    pokemonName.innerHTML = name;
+    document.body.append(pokemonName)
+
+    const pokemonType = document.createElement("div");
+    pokemonType.innerHTML = type;
+    document.body.append(pokemonType);
+    
 }
 
 fetchPokemon();

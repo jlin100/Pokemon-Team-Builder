8abf221782327f6caed6b846732721e0501474b3
Jeff Lin <thejefflin@gmail.com>
2022-05-24 00:01:21 -0400

    toggle for dark mode completed

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 
-const colorSwitch = document.getElementById("inputColorSwitch");
-console.log(colorSwitch);
-// if (colorSwitch) {
-//     colorSwitch.addEventListener("click", () => {
-//         console.log("clicked")
-//     });
-
-
-
 async function fetchPokemon() {
     for(let i = 1; i <= 151; i++) {
         await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
@@ -46,20 +37,20 @@ function searchBar(pokemon) {
     })
     displayPokemon(pokemon)
 } 
-    
+
 function displayPokemon(pokemon) {
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
     const id = `#${pokemon.id.toString().padStart(3, "0")}`;
     const type = (pokemon.type[0].toUpperCase() + pokemon.type.slice(1)).split(",")[0];
-
+    
     const allPokemonContainer = document.getElementById("allPokemonContainer");
     const pokemonContainer = document.createElement("div")
     pokemonContainer.setAttribute("id", "pokemonContainer")
-
+    
     const img = document.createElement("img");
     img.setAttribute("id", "pokemonImage");
     img.src = pokemon.image;
-
+    
     const pokemonID = document.createElement("div");
     pokemonID.setAttribute("id", "pokemonID");
     pokemonID.innerHTML = id;
@@ -67,12 +58,35 @@ function displayPokemon(pokemon) {
     const pokemonName = document.createElement("div");
     pokemonName.setAttribute("id", "pokemonName");
     pokemonName.innerHTML = name;
-
+    
     const pokemonType = document.createElement("div");
     pokemonType.setAttribute("id", "pokemonType");
     pokemonType.innerHTML = type; 
-
+    
     pokemonContainer.append(img, pokemonID, pokemonName, pokemonType);
     allPokemonContainer.appendChild(pokemonContainer);
-
+    
 }
+
+const colorSwitch = document.getElementById("inputColorSwitch");
+
+function toggleColor() {
+        colorSwitch?.addEventListener("click", () => {
+            if(colorSwitch.checked) {
+                darkModeOn();
+            }
+            else {
+                darkModeOff();
+            }
+
+    })   
+}
+ toggleColor();   
+
+ function darkModeOn() {
+     document.body.classList.add("dark-mode");
+ }
+
+ function darkModeOff() {
+    document.body.classList.remove("dark-mode");
+ }
